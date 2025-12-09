7a1fcef869683e3543d93a03acdd29518354fac2
andreas <andreas.bga@gmail.com>
2025-12-09 13:11:34 +0100

    add bitget documentation uta

---
+++ bitget-java-sdk-api/doc/downloadBitgetApiDocs.sh
@@ -0,0 +1,8 @@
+wget \
+  --mirror \
+  --convert-links \
+  --adjust-extension \
+  --page-requisites \
+  --no-parent \
+  --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36" \
+  https://www.bitget.com/api-doc/uta/intro
