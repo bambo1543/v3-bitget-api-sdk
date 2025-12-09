2790e852e6bd3010b8e5d9365828aea542421e3a
andreas <andreas.bga@gmail.com>
2025-12-09 13:25:18 +0100

    updated UtaMarketApi

--- bitget-java-sdk-api/src/main/java/com/bitget/openapi/api/v3/UtaMarketApi.java
+++ bitget-java-sdk-api/src/main/java/com/bitget/openapi/api/v3/UtaMarketApi.java
@@ -12,4 +12,46 @@ public interface UtaMarketApi {
     @GET("/api/v3/market/instruments")
     Call<ResponseResult> instruments(@QueryMap Map<String, String> paramMap);
 
+    @GET("/api/v3/market/tickers")
+    Call<ResponseResult> tickers(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/orderbook")
+    Call<ResponseResult> orderBook(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/fills")
+    Call<ResponseResult> fills(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/proof-of-reserves")
+    Call<ResponseResult> proofOfReserves(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/open-interest")
+    Call<ResponseResult> openInterest(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/candles")
+    Call<ResponseResult> candles(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/history-candles")
+    Call<ResponseResult> historyCandles(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/current-fund-rate")
+    Call<ResponseResult> currentFundingRate(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/history-fund-rate")
+    Call<ResponseResult> historyFundingRate(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/risk-reserve")
+    Call<ResponseResult> riskReserve(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/discount-rate")
+    Call<ResponseResult> discountRate(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/margin-loans")
+    Call<ResponseResult> marginLoans(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/position-tier")
+    Call<ResponseResult> positionTier(@QueryMap Map<String, String> paramMap);
+
+    @GET("/api/v3/market/oi-limit")
+    Call<ResponseResult> openInterestLimit(@QueryMap Map<String, String> paramMap);
+
 }
--- bitget-java-sdk-api/src/main/java/com/bitget/openapi/service/v3/UtaMarketService.java
+++ bitget-java-sdk-api/src/main/java/com/bitget/openapi/service/v3/UtaMarketService.java
@@ -19,4 +19,60 @@ public class UtaMarketService {
     public ResponseResult instruments(Map<String, String> paramMap) throws IOException {
         return ResponseUtils.handleResponse(utaMarketApi.instruments(paramMap).execute().body());
     }
+
+    public ResponseResult tickers(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.tickers(paramMap).execute().body());
+    }
+
+    public ResponseResult orderBook(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.orderBook(paramMap).execute().body());
+    }
+
+    public ResponseResult fills(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.fills(paramMap).execute().body());
+    }
+
+    public ResponseResult proofOfReserves(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.proofOfReserves(paramMap).execute().body());
+    }
+
+    public ResponseResult openInterest(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.openInterest(paramMap).execute().body());
+    }
+
+    public ResponseResult candles(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.candles(paramMap).execute().body());
+    }
+
+    public ResponseResult historyCandles(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.historyCandles(paramMap).execute().body());
+    }
+
+    public ResponseResult currentFundingRate(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.currentFundingRate(paramMap).execute().body());
+    }
+
+    public ResponseResult historyFundingRate(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.historyFundingRate(paramMap).execute().body());
+    }
+
+    public ResponseResult riskReserve(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.riskReserve(paramMap).execute().body());
+    }
+
+    public ResponseResult discountRate(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.discountRate(paramMap).execute().body());
+    }
+
+    public ResponseResult marginLoans(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.marginLoans(paramMap).execute().body());
+    }
+
+    public ResponseResult positionTier(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.positionTier(paramMap).execute().body());
+    }
+
+    public ResponseResult openInterestLimit(Map<String, String> paramMap) throws IOException {
+        return ResponseUtils.handleResponse(utaMarketApi.openInterestLimit(paramMap).execute().body());
+    }
 }
