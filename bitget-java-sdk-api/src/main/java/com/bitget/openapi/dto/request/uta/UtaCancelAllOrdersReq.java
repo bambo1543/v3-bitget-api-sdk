b83fdecd9944cb22d78c41f6f911404322d1281b
bambo1543 <andreas@bambo.it>
2025-12-09 09:36:35 +0100

    Use Lombok builders for UTA request DTOs

--- bitget-java-sdk-api/src/main/java/com/bitget/openapi/dto/request/uta/UtaCancelAllOrdersReq.java
+++ bitget-java-sdk-api/src/main/java/com/bitget/openapi/dto/request/uta/UtaCancelAllOrdersReq.java
@@ -1,47 +1,17 @@
 package com.bitget.openapi.dto.request.uta;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder(toBuilder = true)
 public class UtaCancelAllOrdersReq {
 
     private String symbol;
 
     private String marginCoin;
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public Builder toBuilder() {
-        return new Builder()
-                .symbol(symbol)
-                .marginCoin(marginCoin);
-    }
-
-    public static final class Builder {
-        private String symbol;
-        private String marginCoin;
-
-        private Builder() {
-        }
-
-        public Builder symbol(String symbol) {
-            this.symbol = symbol;
-            return this;
-        }
-
-        public Builder marginCoin(String marginCoin) {
-            this.marginCoin = marginCoin;
-            return this;
-        }
-
-        public UtaCancelAllOrdersReq build() {
-            return new UtaCancelAllOrdersReq(symbol, marginCoin);
-        }
-    }
 }
